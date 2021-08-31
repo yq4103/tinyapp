7ed1cf329f6a5f0265e35ad276592b53382a38ba
Yirong Qin <yq4103@mun.ca>
2021-08-31 01:40:44 -0400

    have to add http before the url for now

--- express_server.js
+++ express_server.js
@@ -1,13 +1,55 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+//The body-parser library will convert the request body from a Buffer into string that we can read.
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+//function to generate a random string for short url
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//when express server receives a POST request to /urls it responds with a redirection to /urls/:shortURL, where shortURL is the random string we generated
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  const shortURL = generateRandomString();
+  const url = req.body;
+  urlDatabase[shortURL] = url.longURL;
+  res.redirect("/urls/" + shortURL);
+});
+
+//Redirect any request to "/u/:shortURL" to its longURL
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+//can get the correct page if add https:// infront of the url.
+
+//render urls_new.ejs
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//render urls_index.ejs
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+//render urls.show.ejs
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
