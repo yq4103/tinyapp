2062463af48e9afb8d3b1b16a744f3470ac04838
Yirong Qin <yq4103@mun.ca>
2021-09-03 01:35:25 -0400

    npm test

---
+++ test/helpersTest.js
@@ -0,0 +1,24 @@
+const { assert } = require('chai');
+
+const { findUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('findUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = findUserByEmail("user@example.com", testUsers);
+    const expectedOutput = "userRandomID";
+    assert.equal(user.id, expectedOutput, "The getUserByEmail function returns a user object when it's provided with an email that exists in the database.");
+  });
+});
